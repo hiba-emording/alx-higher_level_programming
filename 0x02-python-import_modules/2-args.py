8c63c15eb4365eaa40e48e9ca041acf7f961beac
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-11-30 04:12:04 -0800

    a program that prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    counter = len(argv) - 1
+
+    if counter == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(counter))
+        for args in range (counter):
+            print("{} : {}".format(args + 1, argv[args+1]))
+
+
