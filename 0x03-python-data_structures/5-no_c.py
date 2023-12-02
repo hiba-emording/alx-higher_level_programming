b51925779fc2e8946ae763f1308c447b7ec85903
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-12-02 05:44:19 -0800

    Better solution

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = ''
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string = new_string + char
+    translation_table = {ord(char): None for char in 'cC'}
+    new_string = my_string.translate(translation_table)
     return new_string
