2e4c9c2091253195f051318f53012a4717ffb8aa
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-12-04 01:37:47 -0800

    CPython #0: Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,21 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+long int size = PyList_Size(p);
+int i;
+const char *type_name;
+PyListObject *list_object = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size);
+
+	printf("[*] Allocated = %li\n", list_object->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		type_name = Py_TYPE(list_object->ob_item[i])->tp_name;
+		printf("Element %i: %s\n", i, type_name);
+	}
+}
