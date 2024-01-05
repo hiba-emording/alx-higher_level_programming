cb1e2ddc8990e18bc4218c1acb4a412bd8f359a1
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-01-05 07:14:27 -0800

    pycodestyle and documintation fixed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+Defines a class: LockedClass
+"""
+
+
 class LockedClass:
+    """represents a locked class - only first_name attribute is allowed"""
     def __setattr__(self, name, value):
         if name != "first_name":
-            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
+            raise AttributeError(
+                    f"'LockedClass' object has no attribute '{name}'")
         super().__setattr__(name, value)
