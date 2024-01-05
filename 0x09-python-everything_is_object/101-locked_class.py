c873c7910977ee40163c2a7cdbff9d02577cb9fb
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-01-05 07:17:44 -0800

    another approach using slots

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,8 +6,4 @@ Defines a class: LockedClass
 
 class LockedClass:
     """represents a locked class - only first_name attribute is allowed"""
-    def __setattr__(self, name, value):
-        if name != "first_name":
-            raise AttributeError(
-                    f"'LockedClass' object has no attribute '{name}'")
-        super().__setattr__(name, value)
+    __slots__ = ["first_name"]
