e899e725830110a5ae623b7904030c6e3507b213
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-01-08 06:20:12 -0800

    a sub class to print a sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+MODULE: MY LIST
+This module contains the MyList class, a subclass of list,
+with a method to print a sorted list.
+"""
+
+
+class MyList(list):
+    """
+    Class: list
+    Subclass: MyList
+     - Contains a public instance method to
+       print a list sorted in ascending order.
+    """
+    def __init__(self):
+        """ A function for object initialization"""
+        super().__init__()
+
+    def print_sorted(self):
+        """ A function to sort the list and print it"""
+        print(sorted(self))
