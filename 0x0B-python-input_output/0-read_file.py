df17eb8dc6b9fc07315f426293564dabe3de733f
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-01-09 09:01:22 -0800

    File Reader

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+File Reader Module
+contains a function that reads a text file (UTF8) and prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """A function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,4 @@
+## STARTED WITH PYTHON
+### PROJECT NUMBER 11
+
+# 0x0B. Python - Input/Output
