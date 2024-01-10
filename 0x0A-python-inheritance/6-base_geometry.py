031b5014a0ab670a2f8014973dbe82ec28639113
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-01-10 04:19:58 -0800

    added non implemented function area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A module defines a class: BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ represents a class: BaseGeometry """
+    def area(self):
+        """ Not implemented function that raises an exception"""
+        raise Exception("area() is not implemented")
