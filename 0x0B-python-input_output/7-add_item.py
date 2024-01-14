a4b65da71707674de5e580f2f6b908d3106c837b
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-01-14 03:44:41 -0800

    trial for 7 add item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Add items to a list and save to JSON file Module
+"""
+import sys
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    my_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, 'add_item.json')
