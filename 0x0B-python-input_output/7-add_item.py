9a78abc37f8365a612fe3bec118544a439cbbfa7
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-01-14 03:50:51 -0800

    Error fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,17 +3,39 @@
 Add items to a list and save to JSON file Module
 """
 import sys
+import json
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+def save_to_json_file(my_obj, filename):
+    """
+    Writes an object using JSON repr to a text file.
 
-try:
-    my_list = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    my_list = []
+    Args:
+    - my_obj: The object to be written to the file.
+    - filename (str): The name of the file.
+    """
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
+
+
+def load_from_json_file(filename):
+    """
+    Creates/Loads an object from a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file.
+
+    Returns:
+        The object loaded from the JSON file.
+    """
+    try:
+        with open(filename, 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return None
+
+
+my_list = load_from_json_file('add_item.json') or []
 
 my_list.extend(sys.argv[1:])
 
