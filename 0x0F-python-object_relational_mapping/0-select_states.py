6acb249d40d234cf963c08ec73c2d4ccd0a79a97
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-03-16 04:53:36 -0700

    get && filter states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                                 password=sys.argv[2], database=sys.argv[3], port=3306)
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM states")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    connection.close()
