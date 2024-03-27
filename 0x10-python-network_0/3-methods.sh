3d228f8eda2bfe0278efb765f53347c2b4826a7a
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-03-27 11:49:41 -0700

    Number of lines fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# A script that takes in a URL,
-# 	and displays all HTTP methods the server will accept.
-
+# A script that takes in a URL, and displays all HTTP methods the server will accept.
 curl -sI "$1" | grep "ALlow" | cut -d " " -f 2-
