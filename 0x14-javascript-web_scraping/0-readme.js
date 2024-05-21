c406c0618afa8956217af8ed4ea1daaf80eeb8ca
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-05-21 06:30:17 -0700

    Try task 0 with no format

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,8 +5,7 @@ const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', function (error, data) {
   if (error) {
-    const formattedError = `{ Error: ${error.message}\n    at ${error.stack.split('\n')[1]}\n  errno: ${error.errno},\n  code: '${error.code}',\n  syscall: '${error.syscall}',\n  path: '${error.path}' }`;
-    console.error(formattedError);
+    console.log(error);
   } else {
     console.log(data);
   }
