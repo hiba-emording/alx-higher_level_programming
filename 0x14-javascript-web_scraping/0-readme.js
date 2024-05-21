4d3eefbbb6d8b636fe7780831b8cc04545229c34
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-05-21 04:45:59 -0700

    Error format

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,5 +4,10 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', function (error, data) {
-  console.error(error || data);
+  if (error) {
+    const formattedError = `{ Error: ${error.message}\n    at ${error.stack.split('\n')[1]}\n  errno: ${error.errno},\n  code: '${error.code}',\n  syscall: '${error.syscall}',\n  path: '${error.path}' }`;
+    console.error(formattedError);
+  } else {
+    console.log(data);
+  }
 });
